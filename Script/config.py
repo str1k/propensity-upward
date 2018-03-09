c39cb6703d6af25789b565949cbf7a31d9625bc4
str1k <y.thanasit@gmail.com>
2018-03-09 14:08:56 +0700

    add script to git

---
+++ Script/config.py
@@ -0,0 +1,6 @@
+date_stamp = 'test_20180309'
+
+cvm_non_voice_arpu_source = 'wasb://ds-cvm-hd-rs-devprod-02-2017-09-25t08-15-40-207z@natds201708cvm1sa01.blob.core.windows.net/data_cvm/NON_VOICE/cvm_non_voice_arpu_upward_jan18_aug17_20180226.dat'
+cvm_non_voice_arpu_delimiter = '|'
+
+preprocess_01_output_01 = '/preprocessed_cvm/mainset_elim_null_' + date_stamp
