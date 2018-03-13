6b4afd985f6895a1258a0a47ed624e802e9f7ce2
str1k <y.thanasit@gmail.com>
2018-03-13 23:37:37 +0700

    edit downloadBlob

--- Script/downloadBlob.py
+++ Script/downloadBlob.py
@@ -3,12 +3,14 @@ import sys
 import os
 data_dir = '/home/strikermx/data_dir/model_' + sys.argv[2]
 block_blob_service = BlockBlobService(account_name='natds201801cvmarpu', account_key='melH7xjBqGc0yCtz4eL+v8rfR+Lx/cbTqlZ7Jz+adMNpTEIDdAU0L0nd2yUaMkimqU0gM0XixAwk8CRhuKoduw==')
+from azure.storage.blob import PublicAccess
+block_blob_service.create_container('ds-cvm-hdi-arpu-upward-2018-01-12t03-00-38-195z', public_access=PublicAccess.Container)
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
-print(sys.argv[1] + sys.argv[2])
+print(sys.argv[1] )
 generator = block_blob_service.list_blobs('ds-cvm-hdi-arpu-upward-2018-01-12t03-00-38-195z')
 for blob in generator:
-    if sys.argv[1] + sys.argv[2] +'/part' in blob.name:
+    if sys.argv[1] +'/part' in blob.name:
         print(blob.name)
         block_blob_service.get_blob_to_path('ds-cvm-hdi-arpu-upward-2018-01-12t03-00-38-195z', blob.name ,'/home/strikermx/data_dir/model_'+sys.argv[2]+'/training_'+sys.argv[2]+'.dat')
 
