838f9ff55992370b89bad9eaf394849d0fd62ceb
str1k <y.thanasit@gmail.com>
2018-05-16 10:42:30 +0700

    join 2 source

--- Script/ds_config.py
+++ Script/ds_config.py
@@ -6,6 +6,7 @@ devprod02_cluster = 'wasb://ds-cvm-hd-rs-devprod-02-2017-09-25t08-15-40-207z@nat
 cvm_non_voice_arpu_source = 'wasb://ds-cvm-hd-rs-devprod-02-2017-09-25t08-15-40-207z@natds201708cvm1sa01.blob.core.windows.net/data_cvm/NON_VOICE/CVM_ARPU_UPWARD_201803.txt'
 #cvm_non_voice_arpu_source = '/preprocessed_cvm/raw_20180322'
 cvm_non_voice_arpu_delimiter = '|'
+cvm_non_voice_arpu_source_extend = 'wasb://ds-cvm-hd-rs-devprod-02-2017-09-25t08-15-40-207z@natds201708cvm1sa01.blob.core.windows.net/data_cvm/NON_VOICE/CVM_ARPU_UPWARD_SAS_201803.txt'
 
 master_tarif = '/data_cvm/NON_VOICE/master_package_tariff_20180305.txt'
 
